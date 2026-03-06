424808f087c6b201539f56ede25cd21d333c0b83
tiimuAdaptive <81987736+tiimupitt@users.noreply.github.com>
2026-03-06 16:23:56 +0000

    use alternative deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/portfolio",
   output: "export",
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
