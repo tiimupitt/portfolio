ae1ff82b64ee7dd415866f6c128769c0796eba43
tiimuAdaptive <81987736+tiimupitt@users.noreply.github.com>
2026-03-24 12:40:50 +0000

    change base font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next'
 
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Google_Sans_Flex } from 'next/font/google'
 
 import './global.css'
 
-const geistSans = Geist({
+const googleSansFlex = Google_Sans_Flex({
+  axes: ['GRAD', 'ROND', 'wdth', 'opsz', 'slnt'],
   subsets: ['latin'],
-  variable: '--font-geist-sans',
-})
-
-const geistMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-geist-mono',
 })
 
 export const metadata: Metadata = {
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={googleSansFlex.className}
       >
         {children}
       </body>
