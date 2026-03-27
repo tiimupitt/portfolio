67fa1adbeb1198e44a55fb06345e35310e48dbe0
tiimuAdaptive <81987736+tiimupitt@users.noreply.github.com>
2026-03-27 18:51:12 +0000

    improve sidebar layout in extended dimensions

--- app/ui/sidebar/index.tsx
+++ app/ui/sidebar/index.tsx
@@ -11,26 +11,30 @@ export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <EmbossedLeather />
-      <div className={styles.content}>
-        <div className={styles.header}>
-          <h1>Tiimu Pitt</h1>
-          <span>Software Engineer</span>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <h1>Tiimu Pitt</h1>
+            <span>Software Engineer</span>
+          </div>
+          <div className={styles.contact}>
+            <strong>tiimupitt@gmail.com</strong>
+            <span>Brighton, East Sussex</span>
+          </div>
+          <div className={styles.links}>
+            <a href="https://github.com/tiimupitt" target="_blank">
+              <GithubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/tiimu-pitt/" target="_blank">
+              <LinkedinIcon />
+            </a>
+          </div>
         </div>
-        <div className={styles.contact}>
-          <strong>tiimupitt@gmail.com</strong>
-          <span>Brighton, East Sussex</span>
-        </div>
-        <div className={styles.links}>
-          <a href="https://github.com/tiimupitt" target="_blank">
-            <GithubIcon />
-          </a>
-          <a href="https://www.linkedin.com/in/tiimu-pitt/" target="_blank">
-            <LinkedinIcon />
-          </a>
-        </div>
-        <div className={styles.polaroid}>
-          <div>
-            <Image alt="picture of me" src={MeImage} />
+        <div className={styles['polaroid-container']}>
+          <div className={styles['polaroid-paper']}>
+            <div className={styles['polaroid-image']}>
+              <Image alt="picture of me" src={MeImage} />
+            </div>
           </div>
         </div>
       </div>
