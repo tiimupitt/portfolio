ea673002508ee953092741d5cc7bab14d7208bea
Tiimu Pitt <tiimupitt@gmail.com>
2026-03-30 21:35:45 +0100

    add light switch

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import styles from '@/app/styles.module.css'
 import FilePile from '@/app/ui/file-pile'
 import Sidebar from '@/app/ui/sidebar'
 
+import LightSwitch from './ui/light-switch'
+
 export default function Page() {
   return (
     <div className={styles.page}>
@@ -13,6 +15,7 @@ export default function Page() {
         <div className={styles.wrapper}>
           <Image alt="wood background" className={styles.wood} src={WoodImage} />
           <FilePile />
+          <LightSwitch />
         </div>
       </main>
     </div>
---
+++ app/ui/light-switch/index.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import Image from 'next/image'
+import { FunctionComponent, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+import LightSwitchBeadsImage from '@/app/image/light-switch-beads.png'
+import LightSwitchHandleImage from '@/app/image/light-switch-handle.png'
+
+import styles from './styles.module.css'
+
+const LightSwitch: FunctionComponent = () => {
+  gsap.registerPlugin(useGSAP)
+  const blackoutRef = useRef<HTMLDivElement>(null)
+  const switchRef = useRef<HTMLDivElement>(null)
+  const [isLightOn, setIsLightOn] = useState(true)
+
+  useGSAP(() => {
+    gsap.set(blackoutRef.current, { background: 'radial-gradient(circle at center, transparent, transparent 15rem, transparent 20rem, transparent 25rem)', duration: 0.3, ease: 'expo.out', opacity: 0 })
+  }, { scope: blackoutRef })
+
+  const handleMouseMove = (event: globalThis.MouseEvent) => {
+    const { pageX, pageY } = event
+
+    gsap.to(blackoutRef.current, { duration: 1, ease: 'expo.out', left: pageX, top: pageY })
+  }
+
+  const handleToggleLightSwitch = () => {
+    gsap
+      .timeline()
+      .to(switchRef.current, { duration: 0.2, ease: 'expo.inOut', onComplete: () => setIsLightOn(!isLightOn), y: '10rem' })
+      .to(switchRef.current, { duration: 1, ease: 'elastic.out', y: 0 })
+
+    if (isLightOn) {
+      gsap
+        .timeline()
+        .to(blackoutRef.current, { background: 'radial-gradient(circle at center, #16100A, #16100A 15rem, #16100A 20rem, #16100A 25rem)', delay: 0.2, duration: 0.3, ease: 'expo.out', opacity: 1 })
+        .to(blackoutRef.current, { background: 'radial-gradient(circle at center, transparent, transparent 15rem, #f7af66a4 20rem, #16100A 25rem)', delay: 0.2, duration: 1.2, ease: 'elastic.inOut' })
+    }
+    else {
+      gsap.to(blackoutRef.current, { background: 'radial-gradient(circle at center, transparent, transparent 15rem, transparent 20rem, transparent 25rem)', delay: 0.2, duration: 0.3, ease: 'expo.out', opacity: 0 })
+    }
+  }
+
+  const handleLightSwitchMouseEnter = () => {
+    gsap.to(switchRef.current, { duration: 0.2, ease: 'expo.inOut', y: '1rem' })
+  }
+
+  const handleLightSwitchMouseLeave = () => {
+    gsap.to(switchRef.current, { duration: 0.2, ease: 'expo.inOut', y: 0 })
+  }
+
+  window.addEventListener('mousemove', handleMouseMove)
+
+  return (
+    <>
+      <div className={isLightOn ? styles['wrapper-light'] : styles['wrapper-dark']} ref={switchRef}>
+        <div className={styles.beads}>
+          <Image alt="" src={LightSwitchBeadsImage} />
+          <Image alt="" src={LightSwitchBeadsImage} />
+          <Image alt="" src={LightSwitchBeadsImage} />
+          <Image alt="" src={LightSwitchBeadsImage} />
+        </div>
+        <button className={styles.handle} onClick={handleToggleLightSwitch} onMouseEnter={handleLightSwitchMouseEnter} onMouseLeave={handleLightSwitchMouseLeave}>
+          <Image alt="" src={LightSwitchHandleImage} />
+        </button>
+      </div>
+      {createPortal(
+        <div className={styles.blackout} ref={blackoutRef} />,
+        document.body,
+      )}
+    </>
+  )
+}
+
+export default LightSwitch
